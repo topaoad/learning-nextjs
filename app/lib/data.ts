0c71d68a4fd9516fcae9796d6cf649dc72dfe136
toproad <tattun0817@gmail.com>
2024-01-04 07:53:51 +0900

    6章まで終了

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,18 +12,18 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  // This is equivalent to in fetch(..., { cache: 'no-store' }).
 
   try {
     // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+    //   Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
