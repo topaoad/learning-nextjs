1b554f465ffa2cd844b8571e2b407e3e9bf1bd2b
toproad <tattun0817@gmail.com>
2024-01-03 16:43:12 +0900

    第６章デプロイ手前まで

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Noto_Serif_JP } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const noto_serif_jp = Noto_Serif_JP({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
