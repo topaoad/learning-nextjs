15311660da142b811373a3b4a3a1a1fc4777b412
toproad <tattun0817@gmail.com>
2024-01-05 08:09:26 +0900

    10章まで

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,15 +2,13 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices()
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className={`${lusitana.className} mb-4 text - xl md: text - 2xl`}>
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
@@ -31,7 +29,7 @@ export default async function LatestInvoices({
                 <div className="flex items-center">
                   <Image
                     src={invoice.image_url}
-                    alt={`${invoice.name}'s profile picture`}
+                    alt={`${invoice.name} 's profile picture`}
                     className="mr-4 rounded-full"
                     width={32}
                     height={32}
@@ -44,21 +42,21 @@ export default async function LatestInvoices({
                       {invoice.email}
                     </p>
                   </div>
-                </div>
+                </div >
                 <p
                   className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
                 >
                   {invoice.amount}
                 </p>
-              </div>
+              </div >
             );
           })}
-        </div>
+        </div >
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
